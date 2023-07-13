db87652d6c25ba93a72ac157f5cb41f58b1d73e7
matt enbar <matthew.enbar@gmail.com>
2023-07-13 15:13:36 -0400

    image modal display css

--- src/components/rover/ImageModal.js
+++ src/components/rover/ImageModal.js
@@ -12,67 +12,54 @@ const ImageModal = (props) => {
   console.log("img modal props", props);
 
   const handleChange = (event, value) => {
-    console.log("handleChange", value, currentIndex);
     setCurrentIndex(value - 1);
     setCurrentImg(props.photos[value - 1]);
   };
 
   return (
     <Modal onClose={props.onClose}>
-      {/* {console.log(props.img.target['__reactProps$qm4km4ggqg'].orignalSrc)} */}
-      <div
-        style={{
-          height: "80vh",
-          margin: "auto",
-          width: "auto",
-          display: "flex",
-          justifyContent: "flex-end",
-          flexDirection: "column-reverse",
-        }}
+      <CloseIcon id="close-btn" sx={{}} onClick={props.onClose} />
+
+      <ImageListItem
+        key={`${currentImg.camera.name}_${props.img.id}`}
+        id={currentImg.id}
+        name={currentImg.camera.name}
+        sx={{ width: "auto" }}
       >
-        <ImageListItem
-          key={`${currentImg.camera.name}_${props.img.id}`}
+        <img
           id={currentImg.id}
-          name={currentImg.camera.name}
-          sx={{ width: "auto" }}
-        >
-          <img
-            id={currentImg.id}
-            orignalSrc={currentImg.img_src}
-            src={`${currentImg.img_src}?w=248&fit=crop&auto=format`}
-            srcSet={`${currentImg.img_src}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt="img"
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={
-              <>
-                CAMERA: {currentImg.camera.name}
-                <br />{" "}
-              </>
-            }
-            subtitle={
-              <>
-                SOL: {currentImg.sol} ID: {currentImg.id}
-              </>
-            }
-          ></ImageListItemBar>
-        </ImageListItem>
-        <br></br>
-        <CloseIcon id="close-btn" sx={{}} onClick={props.onClose} />
-        
-      </div>
-      <ImageListItemBar
-          title={
-            <Stack sx={{ padding: "5px", alignItems: "center" }} spacing={2}>
-              <Pagination
-                defaultPage={currentIndex + 1}
-                onChange={handleChange}
-                count={props.photos.length}
-              />
-            </Stack>
-          }
+          orignalSrc={currentImg.img_src}
+          src={`${currentImg.img_src}?w=248&fit=crop&auto=format`}
+          srcSet={`${currentImg.img_src}?w=248&fit=crop&auto=format&dpr=2 2x`}
+          alt="img"
+          loading="lazy"
+        />
+        <ImageListItemBar
+          sx={{ position: "absolute !important" }}
+          title={`CAMERA: ${currentImg.camera.name}`}
+          subtitle={`SOL: ${currentImg.sol} ID: ${currentImg.id}`}
         ></ImageListItemBar>
+      </ImageListItem>
+
+      <ImageListItemBar
+      sx={{
+        position: "inherit !important"}}
+        title={
+          <Stack
+            sx={{
+              padding: "5px",
+              alignItems: "center",
+            }}
+            spacing={2}
+          >
+            <Pagination
+              defaultPage={currentIndex + 1}
+              onChange={handleChange}
+              count={props.photos.length}
+            />
+          </Stack>
+        }
+      ></ImageListItemBar>
     </Modal>
   );
 };
